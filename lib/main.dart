21630e3a9f83936caa001e0f0d6cba377d58a9e4
Muhamad Andro <49116664+MuhamadHaydar@users.noreply.github.com>
2020-03-24 17:05:56 +0300

    Completed the onboarding screen by using screen introduction from added package.

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,123 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Introduction Screen',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      home: OnBoardingPage()
     );
   }
 
+}
+
+class OnBoardingPage extends StatefulWidget {
+  @override
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+
+  //Store current page
+  int _currentPage = 0;
+
+  //Method is called at the end, When done button clicked.
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => HomePage()),
+    );
+  }
+
+  //Method Creates the widget that represent image.
+  Widget _buildImage(String assetName) {
+    return Align(
+      child: Image.asset('images/$assetName.png',width: 350, height: 300,),
+      alignment: Alignment.bottomCenter,
+    );
+  }
+
+  @override
+  void initState() {
+    //When user comes to onboarding Screen it must be false, because for the other time
+    //User must not see this screen again.
+    isFirstTime == false;
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    //style for body.
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    //Decoration of the background page.
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    //List of colors for indicator.
+    List<Color> indecatorColors = [Colors.red,Colors.yellow,Colors.blue,Colors.green];
+
+    //Widget comes from the dependency. it's used to draw whole of views.
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Hi!",
+          body:
+          "I am Nasrin, I am here to help you for organising and managing your day usefully.",
+          image: _buildImage('nasrin_intro'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Time!",
+          body:
+          "I care of your time sirously, here you can manipulate your time effeciently.",
+          image: _buildImage('nasrin_time'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Tasks!",
+          body:
+          "Is it important? Yea of course, Therefore You can manage your tasks with me.",
+          image: _buildImage('nasrin_order'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Points!",
+          body: 'You can be the best by collect whole of points through the week.',
+          image: _buildImage('nasrin_point'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      onChange: (int page){
+        print('Color Changed');
+        setState(() {
+          _currentPage = page;
+          print(_currentPage);
+        });
+
+      },
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      showNextButton: true,
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeColor: indecatorColors[_currentPage],
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+    );
+  }
 }
 
 
