7f8357f56196ba40027438e35e702ec8b4d49fe8
Muhamad Andro <49116664+MuhamadHaydar@users.noreply.github.com>
2020-03-26 22:38:55 +0300

    Added shared preferences to save the boolean data which we use to decide about screens that we have to open, If we open application for the first time it will turn from true to false, and we store this value and return in the main method when we want to run our application, Before create widgets because it pop error because of time.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,42 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+bool onScreen;
 
+// Here we have to add the future because we need to await for data comes back
+// from shared preferences. We use the boolean data to decide to which screen to
+// open.
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Create a shared preference to store data.
+  SharedPreferences prefs = await SharedPreferences.getInstance();
 
-bool isFirstTime = true;
+  print('get preference');
+// Try reading data from the counter key. If it doesn't exist, return 0.
+  onScreen =  prefs.getBool('onboard') ?? true;
+  print(onScreen);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
     //make a status bar color transparent
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
     );
+
     return MaterialApp(
       title: 'Introduction Screen',
       debugShowCheckedModeBanner: false,
-      home: OnBoardingPage()
+      home: onScreen ? OnBoardingPage() : HomePage(),
     );
   }
-
 }
 
 class OnBoardingPage extends StatefulWidget {
@@ -32,7 +45,6 @@ class OnBoardingPage extends StatefulWidget {
 }
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
-
   //Store current page
   int _currentPage = 0;
 
@@ -46,7 +58,11 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   //Method Creates the widget that represent image.
   Widget _buildImage(String assetName) {
     return Align(
-      child: Image.asset('images/$assetName.png',width: 350, height: 300,),
+      child: Image.asset(
+        'images/$assetName.png',
+        width: 350,
+        height: 300,
+      ),
       alignment: Alignment.bottomCenter,
     );
   }
@@ -55,14 +71,12 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   void initState() {
     //When user comes to onboarding Screen it must be false, because for the other time
     //User must not see this screen again.
-    isFirstTime == false;
+    setPreference();
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     //style for body.
     const bodyStyle = TextStyle(fontSize: 19.0);
 
@@ -76,7 +90,12 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
     );
 
     //List of colors for indicator.
-    List<Color> indecatorColors = [Colors.red,Colors.yellow,Colors.blue,Colors.green];
+    List<Color> indecatorColors = [
+      Colors.red,
+      Colors.yellow,
+      Colors.blue,
+      Colors.green
+    ];
 
     //Widget comes from the dependency. it's used to draw whole of views.
     return IntroductionScreen(
@@ -84,39 +103,39 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
         PageViewModel(
           title: "Hi!",
           body:
-          "I am Nasrin, I am here to help you for organising and managing your day usefully.",
+              "I am Nasrin, I am here to help you for organising and managing your day usefully.",
           image: _buildImage('nasrin_intro'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Time!",
           body:
-          "I care of your time sirously, here you can manipulate your time effeciently.",
+              "I care of your time sirously, here you can manipulate your time effeciently.",
           image: _buildImage('nasrin_time'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Tasks!",
           body:
-          "Is it important? Yea of course, Therefore You can manage your tasks with me.",
+              "Is it important? Yea of course, Therefore You can manage your tasks with me.",
           image: _buildImage('nasrin_order'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Points!",
-          body: 'You can be the best by collect whole of points through the week.',
+          body:
+              'You can be the best by collect whole of points through the week.',
           image: _buildImage('nasrin_point'),
           decoration: pageDecoration,
         ),
       ],
       onDone: () => _onIntroEnd(context),
-      onChange: (int page){
+      onChange: (int page) {
         print('Color Changed');
         setState(() {
           _currentPage = page;
           print(_currentPage);
         });
-
       },
       //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
       showSkipButton: true,
@@ -137,10 +156,17 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       ),
     );
   }
+
+  // Set data to the preference.
+  void setPreference() async {
+    // obtain shared preferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // set value
+    prefs.setBool('onboard', false);
+    print('SetPreference Method');
+  }
 }
-
-
-
 
 class HomePage extends StatelessWidget {
   @override
@@ -151,4 +177,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
